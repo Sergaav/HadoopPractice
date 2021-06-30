bbc07b31e25e8fd1a469121eb2e1d72fa4abd0b4
Sergaav <sergaav@gmail.com>
2021-07-01 01:45:22 +0300

    Mapper Reducer

--- src/main/java/com/savaz/hadoop/WordCount.java
+++ src/main/java/com/savaz/hadoop/WordCount.java
@@ -2,6 +2,7 @@ package com.savaz.hadoop;
 
 import java.io.IOException;
 import java.util.StringTokenizer;
+
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
@@ -47,11 +48,11 @@ public class WordCount {
     }
 
     public static void main(String[] args) throws Exception {
-
         Configuration conf = new Configuration();
         Job job = Job.getInstance(conf, "word count");
         job.setJarByClass(WordCount.class);
         job.setMapperClass(TokenizerMapper.class);
+        job.setCombinerClass(IntSumReducer.class);
         job.setReducerClass(IntSumReducer.class);
         job.setOutputKeyClass(Text.class);
         job.setOutputValueClass(IntWritable.class);
